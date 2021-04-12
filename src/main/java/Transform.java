4935f4b63b236bd0f1d3fe8926581ddd044158f1
imt2017019 <georget.abraham@iiitb.org>
2021-04-12 09:59:30 +0530

    added a sample csv

---
+++ src/main/java/Transform.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.sql.*;
+public class Transform{
+    public static String select_data(){
+        String command="select * from source_data_dump where type=\"csv\"";
+        return "HI";
+
+    }
+}
